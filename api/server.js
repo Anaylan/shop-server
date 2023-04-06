7514006927695809edce9e359ffc7d5530919cc7
Anaylan <sasha.skobelev90@gmail.com>
2023-04-06 23:08:16 +0600

    d

--- api/server.js
+++ api/server.js
@@ -1,11 +1,12 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server");
+const auth = require("json-server-auth");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 // const cors = require("cors");
 // server.use(cors);
 const middlewares = jsonServer.defaults();
-const authMiddle = require("json-server-auth");
+
 server.use(middlewares);
 
 server.use((req, res, next) => {
@@ -15,14 +16,15 @@ server.use((req, res, next) => {
 });
 // Add this before server.use(router)
 // server.use(
-// 	jsonServer.rewriter({
-// 		"/login*": "/users*",
-// 		"/register": "/users",
-// 	})
+// jsonServer.rewriter({
+// 	"/login*": "/users*",
+// 	"/register": "/users",
+// })
 // );
 server.use(router);
-server.use(authMiddle);
+server.use(auth);
 server.listen(3000, () => {
+	// console.log();
 	console.log("JSON Server is running");
 });
 
