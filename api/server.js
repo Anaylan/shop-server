d1671645ce33cfa2e7ae4c5a84dcf32a94f88191
Anaylan <sasha.skobelev90@gmail.com>
2023-04-06 23:18:40 +0600

    d

--- api/server.js
+++ api/server.js
@@ -1,8 +1,8 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server");
-const auth = require("json-server-auth");
+// const auth = require("json-server-auth");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+// const router = jsonServer.router("db.json");
 // const cors = require("cors");
 // server.use(cors);
 const middlewares = jsonServer.defaults();
@@ -14,13 +14,13 @@ server.use((req, res, next) => {
 	res.header("Access-Control-Allow-Headers", "*");
 	next();
 });
-server.use((req, _, next) => {
-	if (req.method === "POST") {
-		req.body.createdAt = Date.now();
-	}
-	// Передаем управление роутеру `JSON Server`
-	next();
-});
+// server.use((req, _, next) => {
+// 	if (req.method === "POST") {
+// 		req.body.createdAt = Date.now();
+// 	}
+// 	// Передаем управление роутеру `JSON Server`
+// 	next();
+// });
 // Add this before server.use(router)
 // server.use(
 // jsonServer.rewriter({
@@ -28,8 +28,8 @@ server.use((req, _, next) => {
 // 	"/register": "/users",
 // })
 // );
-server.use(router);
-server.use(auth);
+// server.use(router);
+// server.use(auth);
 server.listen(3000, () => {
 	// console.log();
 	console.log("JSON Server is running");
