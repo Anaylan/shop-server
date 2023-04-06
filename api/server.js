08112bee9105a4bff2a43c60e8b9fb3598cc39ca
Anaylan <sasha.skobelev90@gmail.com>
2023-04-06 21:53:39 +0600

    correct cors

--- api/server.js
+++ api/server.js
@@ -2,11 +2,14 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const cors = require("cors");
-
+// const cors = require("cors");
+// server.use(cors);
 const middlewares = jsonServer.defaults();
 server.use(middlewares);
-server.use(cors);
+server.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	next();
+});
 // Add this before server.use(router)
 server.use(
 	jsonServer.rewriter({
